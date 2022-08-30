042e073a17089155abe907c1fecf85a0d2b4b3b7
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-08-30 09:06:04 +0000

    Add files via upload

--- Person.cpp
+++ Person.cpp
@@ -1,22 +1,14 @@
 #include "Person.h"
-
 #include <string>
 
 void person::setName(string myName) {
   name = myName;
-  return;
 }
 
 string person::getName() { return this->name; }
 
 void person::setSalary(int mySalary) {
   salary = mySalary;
-  return;
 }
 
 int person::getSalary() { return this->salary; }
-
-person::person(int mySalary, string myName) {
-  salary = mySalary;
-  name = myName;
-}
--- Person.h
+++ Person.h
@@ -4,14 +4,13 @@ using namespace std;
 
 class person {
  private:
-  string name;
-  int salary;
+    string name;
+    int salary;
 
  public:
   void setName(string myName);
   string getName();
   void setSalary(int mySalary);
   int getSalary();
-  person(int mySalary, string myName);
   person() : name("invalid"), salary(0){};
 };
