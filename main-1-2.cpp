7ffebcd026826739894e1b8845202182d4c0fcc9
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-08-30 09:10:20 +0000

    Add files via upload

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 
 #include "Person.h"
 
@@ -8,10 +7,10 @@ using namespace std;
 int main() {
   person person1, person2, person3, person4;
 
-  cout << "Person " << person1.getName() << " has salary " << person1.getSalary() << endl;
-  cout << "Person " << person2.getName() << " has salary " << person2.getSalary() << endl;
-  cout << "Person " << person3.getName() << " has salary " << person3.getSalary() << endl;
-  cout << "Person " << person4.getName() << " has salary " << person4.getSalary() << endl;
+  cout << "Person: " << person1.getName() << " has salary " << person1.getSalary() << endl;
+  cout << "Person: " << person2.getName() << " has salary " << person2.getSalary() << endl;
+  cout << "Person: " << person3.getName() << " has salary " << person3.getSalary() << endl;
+  cout << "Person: " << person4.getName() << " has salary " << person4.getSalary() << endl;
 
   return 0;
 }
